12c942cb2fcb2dc1a916eb090717fa848c7c4e58
Sonia <carlos@ubuntu.(none)>
2013-10-08 15:12:26 +0100

    no funciona haml

--- rps.rb
+++ rps.rb
@@ -1,19 +1,20 @@
    require 'rack/request'
    require 'rack/response'
+   require 'haml'
    
    module RockPaperScissors
      class App 
    
-       def initialize(app = nil)
-         @app = app
-         @content_type = :html
-        @defeat = {'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper'}
-        @throws = @defeat.keys
-        @choose = @throws.map { |x| 
-           %Q{ <li><a href="/?choice=#{x}">#{x}</a></li> }
-        }.join("\n")
-        @choose = "<p>\n<ul>\n#{@choose}\n</ul>"
-      end
+		 def initialize(app = nil)
+				 @app = app
+				 @content_type = :html
+			   @defeat = {'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper'}
+				@throws = @defeat.keys
+				@choose = @throws.map { |x| 
+				   %Q{ <li><a href="/?choice=#{x}">#{x}</a></li> }
+				}.join("\n")
+				@choose = "<p>\n<ul>\n#{@choose}\n</ul>"
+		 end
   
       def call(env)
         req = Rack::Request.new(env)  
@@ -34,15 +35,12 @@
   
         res = Rack::Response.new
         res.write <<-"EOS"
-        <html>
-          <title>rps</title>
-          <body>
-            <h1>
-               #{anwser}
-               #{@choose}
-            </h1>
-          </body>
-        </html>
+		%html
+		  %head
+			%title rps		  
+		  %body
+			%h1 \\#{anwser}
+			%h1	\\#{@choose}
         EOS
         res.finish
       end # call
