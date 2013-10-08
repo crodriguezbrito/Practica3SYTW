55d43bd7d6b7d087e22a4c7be48c23f1c3f68425
Sonia <carlos@ubuntu.(none)>
2013-10-08 21:50:51 +0100

    mejoria haml

--- rps.rb
+++ rps.rb
@@ -10,10 +10,6 @@
 				 @content_type = :html
 			   @defeat = {'rock' => 'scissors', 'paper' => 'rock', 'scissors' => 'paper'}
 				@throws = @defeat.keys
-				@choose = @throws.map { |x| 
-				   %Q{ <li><a href="/?choice=#{x}">#{x}</a></li> }
-				}.join("\n")
-				@choose = "<p>\n<ul>\n#{@choose}\n</ul>"
 		 end
   
       def call(env)
@@ -33,13 +29,15 @@
             "Ouch; #{computer_throw} beats #{player_throw}. Better luck next time!"
           end
   
-		engine = Haml::Engine.new File.open("views/index.html.haml").read
-		res = Rack::Response.new 
-		res.write engine.render({}, 
-			:anwser => anwser, 
-			:choose => @choose,
-			:throws => @throws)
-		res.finish
+			engine = Haml::Engine.new File.open("views/index.html.haml").read
+			res = Rack::Response.new
+			res.write engine.render({},
+				:anwser => anwser,
+				:choose => @choose,
+				:throws => @throws,
+				:computer_throw => computer_throw,
+				:player_throw => player_throw)
+			res.finish
       end # call
     end   # App
   end     # RockPaperScissors
